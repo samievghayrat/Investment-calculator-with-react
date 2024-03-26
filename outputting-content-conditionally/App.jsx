5952858ccddf7bd3f3ab4d29ddd4cba5cb9365da
Ghayrat Samiev <81125049+samievghayrat@users.noreply.github.com>
2024-03-26 15:29:43 +0900

    Create App.jsx

---
+++ outputting-content-conditionally/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import {useState} from "react";
+
+function App() {
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue
+            };
+        });
+    }
+
+    const inputIsValid =userInput.duration>0;
+
+    return (
+        <> <Header/>
+            <UserInput  userInput={userInput} onChange = {handleChange}/>
+            {!inputIsValid && <p className="center">Please enter a duration greater than zero </p>}
+            {inputIsValid && <Results input={userInput}></Results>}
+        </>)
+}
+
+export default App
