10aa19c690c27180bd83d56234264c62edee797f
ghayratsamiev <Ghayrat Samiev>
2024-03-26 12:44:27 +0900

    added UserInput.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
 
 function App() {
   return (
-    <Header />
-  )
+   <li> <Header/>
+      <UserInput/>
+   </li>)
 }
 
 export default App
